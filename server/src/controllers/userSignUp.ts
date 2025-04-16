e3bee83ddf3fc6ae641f7cdf1f5a02c0473b3406
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-16 20:56:16 +0530

    Setting up Router, Defined User Schema and worked with interfaces and express Document.Regex pattern fo email validation.

---
+++ server/src/controllers/userSignUp.ts
@@ -0,0 +1,10 @@
+import { Express, Request, Response } from "express";
+
+function userSignup(req:Request,res:Response){
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    
+
+}
---
+++ server/src/models/user.db.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, ObjectId, Document } from "mongoose";
+import {IUser} from '../types/user.interface'
+
+export interface IUserDocument extends IUser,Document{};
+
+const userSchema = new Schema<IUserDocument>({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please fill a valid email address'
+          ],
+    },
+    username:{
+        type: String,
+        required: true,
+        lowecase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const userModel = mongoose.model<IUserDocument>("User",userSchema)
+
+export default userModel;
+
+// module.exports {
+//     userModel
+// }
+
