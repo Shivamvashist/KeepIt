b86c826e586bee1e1574224bf87a7fda0ddb8d22
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-17 14:56:09 +0530

    basic signin working in backend

--- server/src/controllers/userLogin.ts
+++ server/src/controllers/userLogin.ts
@@ -1,5 +1,27 @@
 import express,{ Request,Response } from "express";
+import userModel from '../models/user.db';
 
-export function userLogin( req:Request, res:Response ){
-    
+export async function userLogin( req:Request, res:Response ){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const findUser = await userModel.findOne({
+            username
+        });
+
+        if (findUser){
+            if(password === findUser.password){
+                res.json("Successfully Signed In!")
+            } else {
+                res.json("Incorrect Password")
+            }
+        } else {
+            res.json("Cannot find user!")
+        }
+
+    } catch (error) {
+        res.json("some server side error occurred! - "+error)
+    }
+
 }
