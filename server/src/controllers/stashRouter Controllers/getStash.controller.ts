5ee92ed621cecb9830e0cbf8a3e279856c153346
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-22 09:49:35 +0530

    added error handelling in getStash controller

--- server/src/controllers/stashRouter Controllers/getStash.controller.ts
+++ server/src/controllers/stashRouter Controllers/getStash.controller.ts
@@ -3,17 +3,29 @@ import stashModel from '../../models/stash.db';
 
 export async function getStashItems(req:Request,res:Response){
 
-    const userStashItems = await stashModel.find({
-        userId:(req as any).user.userId,
-    }).populate('userId','username email');
+    try {
+
+        const userId = (req as any).user?.userId;
+
+        if(!userId){
+            res.status(401).json({msg:"Unauthorized, PLease login!"});
+        }
+
+        const userStashItems = await stashModel.find({
+            userId:userId,
+        }).populate('userId','username email');
+    
+        res.status(200).json({
+            items:userStashItems
+        })
+    
+    } catch (error: any) {
+        res.status(500).json({msg:"Internal Server error! ",error:error.message})
+    }
+
+}
 
 //     const content = await Content.find().populate({
 //         path: "userId",
 //         select: "username password"
 // })
-
-    res.json({
-        items:userStashItems
-    })
-
-}
