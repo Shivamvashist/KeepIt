c339e2bccd3d4824761442351a5749f79acfb051
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-30 20:47:40 +0530

    added a /me endpoint to fetch loggedIn user Details

--- server/src/routes/userAuth.routes.ts
+++ server/src/routes/userAuth.routes.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import {userSignup} from '../controllers/userAuth Controllers/userSignup';
 import {userLogin} from '../controllers/userAuth Controllers/userLogin';
 import {userDetails} from '../controllers/userAuth Controllers/userDetails';
+import { userAuth } from '../middlewares/auth.middleware';
 
 export const userAuthRouter = Router();
 
 userAuthRouter.post('/signup',userSignup);
 userAuthRouter.post('/login',userLogin);
-userAuthRouter.get('/user/me',userDetails);
+userAuthRouter.get('/user/me',userAuth,userDetails);
