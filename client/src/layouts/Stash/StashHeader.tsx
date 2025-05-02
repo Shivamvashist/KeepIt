6c726558b4b24fe29954423d126c2cb2718f6d4b
Shivamvashist <ninjavashistsv1@gmail.com>
2025-05-02 19:42:59 +0530

    added Stash Page structure

---
+++ client/src/layouts/Stash/StashHeader.tsx
@@ -0,0 +1,9 @@
+import { motion } from 'framer-motion'
+
+export function StashHeader(){
+
+    return <motion.div >
+        
+    </motion.div>
+    
+}
--- client/src/pages/Stash.tsx
+++ client/src/pages/Stash.tsx
@@ -1,10 +1,36 @@
 import {motion} from 'framer-motion';
+// import { StashHeader } from '../layouts/Stash/StashHeader';
+
 
 export function Stash(){
     
-    return <div className=' min-h-screen min-w-screen bg-gradient-to-b from-[#000000] to-[#150d81]'>
-        <motion.div className=''>
-            <input className='border border-white h-[10%] w-[80%]'></input>
-        </motion.div>
-    </div>
+    return <motion.div className=' w-full h-full bg-black bg-gradient-to-br '>
+
+            <motion.div className='w-[100vw] h-[100vh]'>
+                <motion.div className='flex justify-center items-center pt-40 '>
+                    <motion.span
+                    initial={{y:250,zIndex:20,scale:2}}
+                    animate={{y:[250,-50,0],zIndex:0,scale:1}}
+                    transition={{delay:0.4,duration:0.6,ease:"easeInOut"}}
+                    className='absolute text-gray-600/40 text-[200px] text-center font-bold font-[moonwalk] top-16 bg-black'>
+                        KEEP IT
+                     </motion.span>
+
+                    
+                    <motion.div className='relative flex flex-col w-[60%] pt-20 '>
+                        
+                        <motion.input
+                        initial={{scale:0,opacity:0}}
+                        animate={{scale:1,opacity:1}}
+                        transition={{delay:0.5,duration:0.6,ease:"easeInOut"}}
+                        className='border relative bg-black border-white/20 w-full  h-60 rounded-4xl  focus:outline-none focus:border-blue-500 focus:shadow-md transition-all duration-300'/>
+                        
+
+                    </motion.div>
+                </motion.div>
+                
+
+            </motion.div>
+
+    </motion.div>
 }
