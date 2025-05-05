6f47217da6449df07a7b3d40034dfe5ed9aeb435
Shivamvashist <ninjavashistsv1@gmail.com>
2025-05-06 04:29:47 +0530

    Project up and running

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: process.env.FRONTEND_ORIGIN,credentials: true,}));
+app.use(cors({origin: 'https://keepit-mauve.vercel.app/',credentials: true,}));
 
 app.use('/api/v1/userAuth',userAuthRouter);
 app.use('/api/v1/stash',stashRouter);
