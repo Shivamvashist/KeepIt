4fe61b9fa565f39bc6625ab60a672abc3f09a66d
Shivamvashist <ninjavashistsv1@gmail.com>
2025-05-06 04:37:54 +0530

    cors error headache

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_ORIGIN,credentials: true,}));
 
 app.use('/api/v1/userAuth',userAuthRouter);
 app.use('/api/v1/stash',stashRouter);
